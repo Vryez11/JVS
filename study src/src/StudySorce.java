15d4a2424def86d5ea03c8fd4b51dcce8a3a3094
이지원 <vryez11@gmail.com>
2024-05-02 12:48:28 +0900

    Add StudySorce
    임의 소스 코드 추가

---
+++ study src/src/StudySorce.java
@@ -0,0 +1,12 @@
+public class StudySorce {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+
+        long num1 = 500000000000000000L;
+
+        System.out.println("num = " + num);
+        System.out.println("num1 = " + num1);
+    }
+}
