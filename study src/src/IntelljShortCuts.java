4f4e77b7aeeae0aeef5ff4e96ae4298e766a47f5
이지원 <vryez11@gmail.com>
2024-05-10 12:37:08 +0900

    IntelliJ 단축키 ADD
    IntelliJ 단축키 공부 소스 코드 ADD

---
+++ study src/src/IntelljShortCuts.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class IntelljShortCuts {
+
+    public static void main(String[] args) {
+        String[] arrStr = {"hello", "java", "spring", "world"};
+    }
+
+    public static void printHello() {
+        String hello = "hello";
+        
+    }
+
+}
